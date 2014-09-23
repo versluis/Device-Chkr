eb8c0db3694847efac623cd839879e3ec5c68359
Super_Y <yuanlinyue@gmail.com>
2014-09-23 12:46:09 +0800

    Add iPhone 6, iPhone 6 Plus.

--- DeviceChkr/MainViewController.m
+++ DeviceChkr/MainViewController.m
@@ -123,6 +123,8 @@
     if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5c (GSM, Europe/Asia)";
     if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5s (US/Japan)";
     if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5s (Europe/Asia)";
+    if ([platform isEqualToString:@"iPhone7,1"])    return @"iPhone 6 Plus";
+    if ([platform isEqualToString:@"iPhone7,2"])    return @"iPhone 6";
     
     if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch (1 Gen)";
     if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch (2 Gen)";
